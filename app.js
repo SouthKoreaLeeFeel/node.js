82c6b62de0cc65623cb43024a4978223ce63f8a5
southkorealeefeel <xleefeelx@gmail.com>
2016-08-29 17:48:58 +0900

    Database add

--- app.js
+++ app.js
@@ -1,7 +1,34 @@
 var express = require('express');
 var path = require('path');
 var app = express();
-console.log(__dirname);
+var mongoose = require('mongoose');
+
+mongoose.connect("mongodb://test:1234@ds019076.mlab.com:19076/southkorea");
+var db = mongoose.connection;
+db.once("open", function(){
+  console.log("DB connected!");
+});
+
+db.once("error", function(err){
+  console.log("DB ERROR :", err);
+});
+
+var dataSchema = mongoose.Schema({
+  name:String,
+  count:Number
+});
+var Data = mongoose.model('data',dataSchema);
+Data.findOne({name:"myData"},function(err,data){
+  if(err) return console.log("Data ERROR :  ",err);
+  if(!data){
+    Data.create({name:"myData",count:0},function(err,data){
+      if(err) return console.log("Data ERROR",err);
+      console.log("Counter initialized : ",data);
+    });
+  }
+});
+
+
 /*
 app.get('/',function (req,res){
   res.send('Hello World!');
@@ -9,7 +36,7 @@ app.get('/',function (req,res){
 */
 app.set("view engine",'ejs');
 //app.use(express.static(path.join(__dirname + '/public')));
-
+console.log(__dirname);
 var data={count:0};
 
 app.get('/', function (req,res){
@@ -17,6 +44,8 @@ app.get('/', function (req,res){
   res.render('my_first_ejs',data);
 });
 
+
+/*
 app.get('/reset', function (req,res){
   data.count=0;
   res.render('my_first_ejs',data);
@@ -35,7 +64,7 @@ app.get('/set/:num', function (req,res){
   res.render('my_first_ejs',data);
 });
 
-
+*/
 
 app.listen(3000, function(){
   console.log('Server On!');
